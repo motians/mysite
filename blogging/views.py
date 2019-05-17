8285ba599e932fd27aeb02437c361d5ffba49acb
motians <motians@comcast.net>
2019-05-16 20:59:47 -0700

    added views for detail view

--- blogging/views.py
+++ blogging/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect, Http404
+from blogging.models import Post
+from django.shortcuts import render
 
 
 def stub_view(request, *args, **kwargs):
@@ -10,3 +12,20 @@ def stub_view(request, *args, **kwargs):
         body += "Kwargs:\n"
         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
     return HttpResponse(body, content_type="text/plain")
+
+
+def list_view(request):
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by('-published_date')
+    context = {'posts': posts}
+    return render(request, 'blogging/list.html', context)
+
+
+def detail_view(request, post_id):
+    published = Post.objects.exclude(published_date__exact=None)
+    try:
+        post = published.get(pk=post_id)
+    except Post.DoesNotExist:
+        raise Http404
+    context = {'post': post}
+    return render(request, 'blogging/detail.html', context)
